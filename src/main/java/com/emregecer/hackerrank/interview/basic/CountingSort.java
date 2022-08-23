a8e4bfc0bfadbaa8a1f07ea09412f50665dec5f9
Emre Can Gecer <emre@caunos.com>
2022-08-23 22:05:33 +0300

    Counting Sort

---
+++ src/main/java/com/emregecer/hackerrank/interview/basic/CountingSort.java
@@ -0,0 +1,54 @@
+package com.emregecer.hackerrank.interview.basic;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class CountingSort {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        List<Integer> frequencyArray = IntStream.of(new int[100])
+                .boxed()
+                .collect(Collectors.toList());
+
+        arr.forEach(value -> frequencyArray.set(value, frequencyArray.get(value) + 1));
+
+        return frequencyArray;
+    }
+}
+
